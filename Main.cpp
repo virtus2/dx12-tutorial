3905325725a3909ec49710ebedccf27f6f93ab3d
virtus2 <khd1323@naver.com>
2022-07-08 03:52:27 +0900

    02. Creating a Window

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,122 @@
 #include "stdafx.h"
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
+bool InitializeWindow(HINSTANCE hInstance, int ShowWnd, int width, int height, bool fullscreen)
 {
+	if(fullscreen)
+	{
+		HMONITOR hmon = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+		MONITORINFO mi = { sizeof(mi) };
+		GetMonitorInfo(hmon, &mi);
+
+		width = mi.rcMonitor.right - mi.rcMonitor.left;
+		height = mi.rcMonitor.bottom - mi.rcMonitor.top;
+	}
+	// fill out a WNDCLASSEX structure.
+	// This structure will describe our window.
+	WNDCLASSEX wc;
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = NULL;
+	wc.cbWndExtra = NULL;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 2);
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = WindowName;
+	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	// register the class 
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBox(NULL, L"Error registering class", L"Error", MB_OK | MB_ICONERROR);
+		return false;
+	}
+
+	// create window with registered class
+	hwnd = CreateWindowEx(NULL, 
+		WindowName, 
+		WindowTitle, 
+		WS_OVERLAPPEDWINDOW, 
+		CW_USEDEFAULT, 
+		CW_USEDEFAULT, 
+		width, 
+		height, 
+		NULL, 
+		NULL, 
+		hInstance, 
+		NULL);
+
+	if(!hwnd)
+	{
+		MessageBox(NULL, L"Error creating window", L"Error", MB_OK | MB_ICONERROR);
+		return false;
+	}
+
+	if(fullscreen)
+	{
+		SetWindowLong(hwnd, GWL_STYLE, 0);
+	}
+
+	ShowWindow(hwnd, ShowWnd);
+	UpdateWindow(hwnd);
+
+	return true;
+}
+
+void mainloop()
+{
+	// MSG object will hold any windows message that the PeekMessage function gets.
+	MSG msg;
+	ZeroMemory(&msg, sizeof(MSG));
+
+	while(true)
+	{
+		if(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT) break;
+			// If we find a message, we translate the message then dispatch it.
+			// This is just so windows doesn't think our program stopped responding.
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else
+		{
+			// If we did not find a message, we run our game code and continue the loop.
+		}
+	}
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	// We had to give this function to the windows class when we created it.
+	// This is so we can get windows messages that are sent from Windows, such as key presses, mouse movements, or window resizing.
+	// For now, all we are checking for are the escape key, and if the x button on the top right of the window has been pressed.
+	switch (msg)
+	{
+	case WM_KEYDOWN:
+		if (wParam == VK_ESCAPE)
+		{
+			if (MessageBox(0, L"Are you sure you want to exit?", L"Really?", MB_YESNO | MB_ICONQUESTION) == IDYES)
+				DestroyWindow(hwnd);
+		}
+		return 0;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+	if(!InitializeWindow(hInstance, nShowCmd, Width, Height, FullScreen))
+	{
+		MessageBox(0, L"Window Intialization - Failed", L"Error", MB_OK);
+		return 0;
+	}
+	mainloop();
 	return 0;
 }
--- stdafx.h
+++ stdafx.h
@@ -10,3 +10,28 @@
 #include <d3dcompiler.h>
 #include <DirectXMath.h>
 #include "d3dx12.h"
+
+// Handle to the window
+HWND hwnd = NULL;
+
+// Name of the window
+LPCTSTR WindowName = L"DX12 tutorials";
+
+// Title of the window
+LPCTSTR WindowTitle = L"DX12 Window";
+
+// width and height of the window
+int Width = 800;
+int Height = 600;
+
+// Is window full screen?
+bool FullScreen = false;
+
+// Create a window
+bool InitializeWindow(HINSTANCE hInstance, int ShowWnd, int width, int height, bool fullscreen);
+
+// main application loop
+void mainloop();
+
+// callback function for windows messages;
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
