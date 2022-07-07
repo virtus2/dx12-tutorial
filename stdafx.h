9e50bd69cd18689fc9de53b1cabd3e47db9daf69
virtus2 <khd1323@naver.com>
2022-07-08 03:07:07 +0900

    01. Setting up DirectX12

---
+++ stdafx.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <windows.h>
+#include <d3d12.h>
+#include <dxgi1_4.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+#include "d3dx12.h"
