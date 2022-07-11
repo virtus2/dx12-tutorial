3e751649c7756e7df27a7f2bcabf3e351b71ebe6
virtus2 <khd1323@naver.com>
2022-07-12 03:05:57 +0900

    07. Depth Testing

--- stdafx.h
+++ stdafx.h
@@ -91,6 +91,10 @@ ID3D12Resource* indexBuffer; // a default buffer in GPU memory that we will load
 
 D3D12_INDEX_BUFFER_VIEW indexBufferView; // a structure holding information about the index buffer
 
+ID3D12Resource* depthStencilBuffer; // This is the memory for our depth buffer. it will also be used for a stencil buffer in a later tutorial
+
+ID3D12DescriptorHeap* dsDescriptorHeap; // This is a heap for our depth/stencil buffer descriptor
+
 std::wstring GetLatestWinPixGpuCapturerPath();
 
 
